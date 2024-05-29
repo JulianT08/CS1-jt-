041cdbeb3b8fdac658049eef100ae5c12955bf32
JulianT08 <jtoub26@gcds.net>
2024-05-29 09:12:44 -0400

    Add files via upload

---
+++ pseudocode5.20.py
@@ -0,0 +1,20 @@
+'''
+make a function called war (use the variable that asks how many times)
+    print 'lets play war'
+    set numbers to the card numbers
+    set suits to the card suits
+    print all of the possibilities (using for loop)
+    create a card for the user with a random number value and random suit
+    create a card for the bot with a random number value and random suit
+    if the number value of your card is greater than the number value of the bots card
+        print You won!
+    if the number value of your card equals the number value of the bots card
+        print It's a draw
+    if the number value of your card is less than the number value of the bots card
+        print You lose
+
+make a main function
+    ask how many times the user wants to play (store it as an integer)
+    repeat the war function as many times as the user wants 
+
+'''
